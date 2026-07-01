ebe7b351ebe08d78ba6eb15779093e59441a724f
Jonathan Udeh <jonathanudeh06@gmail.com>
2026-07-01 22:24:02 +0100

    Booking model and page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,12 +6,13 @@ const {
   getAccount,
 } = require('../controllers/viewController');
 const { protect, isLoggedIn } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
 router.use(isLoggedIn);
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
