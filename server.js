9c1b8bc185eebab6a7d7be29b39aba9a0a9bd1ca
Jonathan Udeh <jonathanudeh06@gmail.com>
2026-07-04 17:48:53 +0100

    Remove console logs

--- server.js
+++ server.js
@@ -11,17 +11,18 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(process.env.DATABASE);
-console.log(process.env.DATABASE_PASSWORD);
-
 const DB = process.env.DATABASE.replace(
   '<db_password>',
   process.env.DATABASE_PASSWORD,
 );
 
-console.log(DB);
-
-mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
